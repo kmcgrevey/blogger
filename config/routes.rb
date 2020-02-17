c86791fb9815ecb6d726e8dade9ea72bf1aee852
Kevin McGrevey <niv3kmcg@gmail.com>
2020-02-17 11:11:55 -0700

    Add article edit function and testing

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   post '/articles', to: 'articles#create'
   get '/articles/:id', to: 'articles#show'
   delete '/articles/:id', to: 'articles#destroy'
+  get '/articles/:id/edit', to: 'articles#edit'
+  patch '/articles/:id', to: 'articles#update'
+
 end
---
+++ spec/features/articles/edit_an_article_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "Edit an article", type: :feature do
+  describe "when I link from article show" do
+    describe "I edit a field" do
+      it "creates updated article" do
+        article_1 = Article.create!(title: "Title 1", body: "This is a mistake!")
+
+        visit "/articles/#{article_1.id}"
+
+        expect(page).to have_content("This is a mistake!")
+
+        click_on "Edit"
+
+        expect(current_path).to eq("/articles/#{article_1.id}/edit")
+
+        fill_in :title, with: "Changed Title!"
+        fill_in :body, with: "Corrected."
+        click_on "Update Article"
+
+        expect(current_path).to eq("/articles/#{article_1.id}")
+
+        expect(page).to have_content("Changed Title!")
+        expect(page).to have_content("Corrected.")
+        expect(page).to_not have_content("Title 1")
+        expect(page).to_not have_content("This is a mistake!")
+      end
+    end
+  end
+end
